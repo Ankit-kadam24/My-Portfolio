566a65b3353996f2a6e5777ea68e3b12a048e80b
Ankit-kadam24 <ankitkadam1997@gmail.com>
2025-08-02 13:42:23 +0530

    UI Inhancement

--- biodata/script.js
+++ biodata/script.js
@@ -514,3 +514,42 @@ startAutoSlide();
 // });
 
 // // Rest of your existing code...
+
+function calculateAge(birthdate) {
+    var birthdateObj = new Date(birthdate);
+    var currentDate = new Date();
+
+    var ageInMilliseconds = currentDate - birthdateObj;
+
+    var years = Math.floor(ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000));
+    var months = Math.floor((ageInMilliseconds % (365.25 * 24 * 60 * 60 * 1000)) / (30.44 * 24 * 60 * 60 * 1000));
+    var days = Math.floor((ageInMilliseconds % (30.44 * 24 * 60 * 60 * 1000)) / (24 * 60 * 60 * 1000));
+
+    return {
+        years: years,
+        months: months,
+        days: days
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Your birthdate in the format YYYY-MM-DD
+    var birthdate = '1997-03-26';
+    var dateofJoining = '2021-02-15';
+
+    // Calculate age
+    var age = calculateAge(birthdate);
+
+    var expirence = calculateAge(dateofJoining);
+
+    // Update the age in the HTML
+    document.getElementById('age').textContent = age.years;
+    // document.getElementById('age').textContent = age.years + ' years ' + age.months + ' months ' + age.days + ' days';
+
+    document.getElementById('experience').textContent = expirence.years + ' years ' + expirence.months + ' months ';
+
+
+    //Getting current year to print in Copyrigh
+    var currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+});
